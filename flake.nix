66e7adc3352fcf32cef9440ced56bbc784d7545c
Samuel Monson <smonson@redhat.com>
2024-08-07 17:27:47 -0400

    Nixify

---
+++ flake.nix
@@ -0,0 +1,32 @@
+{
+  description = "RAG Demo";
+
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
+    flake-utils.url = "github:numtide/flake-utils";
+    poetry2nix = {
+      url = "github:nix-community/poetry2nix";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
+  };
+
+  outputs = { self, nixpkgs, flake-utils, poetry2nix, ... }:
+    flake-utils.lib.eachDefaultSystem (system:
+      let
+        pkgs = nixpkgs.legacyPackages.${system};
+        # see https://github.com/nix-community/poetry2nix/tree/master#api for more functions and examples.
+        inherit (poetry2nix.lib.mkPoetry2Nix { inherit pkgs; }) mkPoetryApplication;
+
+        pgEnv = pkgs.postgresql_16_jit.withPackages (pgpkgs: with pgpkgs; [pgvector]);
+      in
+      {
+        packages.default = mkPoetryApplication { projectDir = ./.; };
+
+        devShells.default = pkgs.mkShell {
+          #inputsFrom = [ self.packages.${system}.default ];
+          packages = with pkgs; [ poetry pyright pgEnv ];
+          # Execs the current shell
+          #shellHook = ''exec zsh'';
+        };
+      });
+}
