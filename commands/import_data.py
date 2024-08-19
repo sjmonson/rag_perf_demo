edf45bd0f844383f75a4128448224c0a491c9a21
Samuel Monson <smonson@redhat.com>
2024-08-19 15:47:19 -0400

    Make import docs a list not iterable

--- commands/import_data.py
+++ commands/import_data.py
@@ -3,7 +3,7 @@ from db import VectorDB
 from embedding import read_pdf_file
 
 def import_data(args, model, device, tokenizer):
-    documents = chain(*[ read_pdf_file(doc) for doc in args.data_source ])
+    documents = list(chain(*[ read_pdf_file(doc) for doc in args.data_source ]))
 
     db = VectorDB()
     conn = db.populate_db(documents)
