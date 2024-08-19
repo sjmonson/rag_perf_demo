20f1ec212d2ca36ca9546f770e673876a545b467
Samuel Monson <smonson@redhat.com>
2024-08-19 15:31:28 -0400

    Chain split documents

--- commands/import_data.py
+++ commands/import_data.py
@@ -1,8 +1,9 @@
+from itertools import chain
 from db import VectorDB
 from embedding import read_pdf_file
 
 def import_data(args, model, device, tokenizer):
-    documents = [ read_pdf_file(doc) for doc in args.data_source ]
+    documents = chain(*[ read_pdf_file(doc) for doc in args.data_source ])
 
     db = VectorDB()
     conn = db.populate_db(documents)
